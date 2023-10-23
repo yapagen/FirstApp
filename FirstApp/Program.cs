9d0e39e4e2991326b55119580fd27d5c5bea04a1
yapagen <p.yarlikov@gmail.com>
2023-10-23 19:54:27 +0300

    Урок по переменным

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,11 +10,17 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
 
-            string MyName;
-            MyName = "Jane";
-            Console.WriteLine(MyName)
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
 
+            Console.ReadLine();
         }
     }
 }
